e906eb75e5fb82b8938b41f0372d0dbd5b56dee2
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-15 17:58:32 +0300

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        int clientOS = 0;
+        switch (clientOS) {
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+        }
     }
 
     public static void task2 () {
